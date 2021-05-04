8981cf1f1c3f6d426bdb8547012e04d5b7e853fe
Parker <63526216+MisterJavaScript123@users.noreply.github.com>
2021-05-04 08:21:47 -0500

    Update playercontroller.js

--- src/engine/components/playercontroller.js
+++ src/engine/components/playercontroller.js
@@ -2,10 +2,17 @@ import Input from "../input.js";
 import Component from "./component.js";
 
 export default class PlayerController extends Component {
+	
+	static meshList = [];
+	
 	start () {
 		console.log("Initializing player controller!");
 	}
-
+	
+	attach (gameObject) {
+		this.meshList.push(gameObject);
+	}
+	
 	update () {
 
 		//console.log("Updating player controller!");
